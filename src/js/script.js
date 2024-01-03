f20f9de7951ac8e4a7c6cab332213d1dbb6fe0c4
Wojciech Stancel <wojciech.stancel@gmail.com>
2024-01-03 21:51:06 +0100

    changed path for tests

--- src/js/script.js
+++ src/js/script.js
@@ -150,34 +150,35 @@ Array.prototype.forEach.call(
 	}
 );
 
+// SCROLLSPY
 
-const handleScrollSpy = () => {
-	if (document.body.classList.contains("main-page")) {
-		const sections = [];
+// const handleScrollSpy = () => {
+// 	if (document.body.classList.contains("main-page")) {
+// 		const sections = [];
 
-		allSections.forEach((section) => {
-			let top = window.scrollY;
-			let offset = section.offsetTop - 100;
-			let height = section.offsetHeight;
-			if (top >= offset && top < offset + height) {
-				sections.push(section);
-				const activeSection = document.querySelector(
-					`[href*="${sections[0].id}"]`
-				);
+// 		allSections.forEach((section) => {
+// 			let top = window.scrollY;
+// 			let offset = section.offsetTop - 100;
+// 			let height = section.offsetHeight;
+// 			if (top >= offset && top < offset + height) {
+// 				sections.push(section);
+// 				const activeSection = document.querySelector(
+// 					`[href*="${sections[0].id}"]`
+// 				);
 
-				if (activeSection) {
-					menuItems.forEach((item) => item.classList.remove("nav-active"));
-					activeSection.classList.add("nav-active");
-				} else {
-					menuItems.forEach((item) => item.classList.remove("nav-active"));
-					menuItems[0].classList.add("nav-active");
-				}
-			}
-		});
-	}
-};
+// 				if (activeSection) {
+// 					menuItems.forEach((item) => item.classList.remove("nav-active"));
+// 					activeSection.classList.add("nav-active");
+// 				} else {
+// 					menuItems.forEach((item) => item.classList.remove("nav-active"));
+// 					menuItems[0].classList.add("nav-active");
+// 				}
+// 			}
+// 		});
+// 	}
+// };
 
-window.addEventListener("scroll", handleScrollSpy);
+// window.addEventListener("scroll", handleScrollSpy);
 window.addEventListener("scroll", sectionObserver);
 
 
